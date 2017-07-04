6791bea819c815e2c898698caf509baf3bdc868e
WooBooloveworld <wrenforever@qq.com>
2017-07-04 23:34:01 +0800

    django1

---
+++ mysite/cmdb/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+# Create your views here.
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+
+
+def index(request):
+
+
+	return render(request,'index.html')
+
